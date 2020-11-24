dfb7807df8683f477f2d63f9ff0108c5f3b08565
imgo7 <46975468+imgo7@users.noreply.github.com>
2020-11-24 14:20:12 +0000

    Uploaded 2 Python files and 2 Datasets
    2 Python files for logisitic regression and 2 data CSV files.

---
+++ logreg-model1.py
@@ -0,0 +1,5 @@
+# Model 1 Logistic Regression
+
+# Case:
+# Households in the Small Area have Internet Access taking
+# into account PC instance and level of education.
---
+++ logreg-model2.py
@@ -0,0 +1,5 @@
+# Model 2 Logistic Regression
+
+# Case:
+# Measuring if the level of education in the Small Area is influenced
+# by single motor car ownership and Internet Access.
