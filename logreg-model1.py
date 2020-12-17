ff96376ab30860194c1bb27e56dc91b90917c9a8
imgo7 <46975468+imgo7@users.noreply.github.com>
2020-12-17 10:11:36 +0000

    Column added with +1 and -1 for target variable
    Classified each target column value with +1 (if the value of the target column - Internet Access Broadband - was over 50%) and -1  (if the value of the target column - Internet Access Broadband - was under 50%). Prints to terminal.

--- logreg-model1.py
+++ logreg-model1.py
@@ -9,6 +9,24 @@ import pandas as pds
 # Import numpy for maths functions 
 import numpy as npy 
 
+# Add function to classify Broadband
+# Target Variable column produced 
+# +1 if over 50%, -1 if under 50%
+# Input - internet level percentage
+def assign_tv(internet_level_perc):
+
+    # +1 if over 50%
+    if internet_level_perc >= 50.00:
+        return 1
+    # -1 if under 50%
+    elif internet_level_perc <= 50.00:
+        return -1
+    # invalid number
+    else:
+        return 0
+
+
+
 # Read in the data for the first model 
 # Feature 1 = % of Households with a PC
 # Target Variable = % of Households with Broadband Internet Access
@@ -33,9 +51,36 @@ dataset_1 = pds.concat([dataset_part1,dataset_part2],axis=1)
 
 # Reorder columns : features, target variable
 dataset_1 = dataset_1[['Perc_Households_With_Personal_Computer_Yes_2011','Perc_Persons_15_And_Over_Edu_Not_Ceased_Total_At_School_University_2011','Perc_Households_With_Internet_Access_Broadband_2011']]
-print(dataset_1)
+# print(dataset_1)
 # print(dataset_1['Perc_Persons_15_And_Over_Edu_Not_Ceased_Total_At_School_University_2011'])
 
 # Rename columns
 dataset_1 = dataset_1.rename(columns={'Perc_Households_With_Personal_Computer_Yes_2011':'PC in Household','Perc_Persons_15_And_Over_Edu_Not_Ceased_Total_At_School_University_2011':'Education Level','Perc_Households_With_Internet_Access_Broadband_2011':'Internet Access Broadband' })
 print(dataset_1)
+
+# column = dataset_1['Internet Access Broadband']
+
+# Add list to store the target variables 
+tv_list = []
+
+
+# Assign +1 or -1 for the target variable 
+for x in range(18489):
+    # select each value in Internet Broadband
+    target_var = dataset_1['Internet Access Broadband'][x]
+    # print("Column:", target_var)
+
+    # send to function
+    # to get +1 or -1
+    target_value = assign_tv(target_var)
+
+    # Add the value to a list 
+    tv_list.append(target_value)
+
+# Add new column with +1 and -1 markers 
+dataset_1['Internet Broadband - target variable'] = tv_list
+# Includes new column
+print(dataset_1)
+
+    
+
