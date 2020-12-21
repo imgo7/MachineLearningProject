9e3362a232beeb0b8427746f11061928ebc97177
imgo7 <46975468+imgo7@users.noreply.github.com>
2020-12-21 09:24:34 +0000

    Mean absolute error for Model 2
    Prints mean absolute error loss to the terminal, as well as the baseline model's mean absolute error in logreg-model2.py.

--- logreg-model2.py
+++ logreg-model2.py
@@ -179,6 +179,11 @@ print(confusion_matrix)
 print("Model 2 - Prediction precision: ")
 print(metrics.precision_score(test_y,predict_y))
 
+# Add mean absolute error for the model
+print("Model 2 - Mean absolute error: ")
+# Mean absolute error - actual values, estimates
+print(metrics.mean_absolute_error(test_y,predict_y))
+
 
 # Baseline model - for comparison
 # Uniform strategy - random predictions at all times
@@ -219,3 +224,8 @@ print(dummy_confusion_matrix)
 # Measure precision and print to terminal
 print("Baseline Model 2 Prediction precision: ")
 print(metrics.precision_score(test_y,dummy_predict_y))
+
+# Add mean absolute error for the baseline model
+print("Baseline Model 2 Mean absolute error: ")
+# Mean absolute error - actual values, estimates
+print(metrics.mean_absolute_error(test_y,dummy_predict_y))
